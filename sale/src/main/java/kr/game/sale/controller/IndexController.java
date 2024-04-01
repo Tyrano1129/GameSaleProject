9ccb1d7550d7c56929af0c5049ad866f4a0f702c
Kimjinsu <145655175+Tyrano1129@users.noreply.github.com>
2024-04-01 11:48:03 +0900

    create
    기본적인 설정 완료 (MySQL spring WEP  lombok) 실행까지 확인함

---
+++ sale/src/main/java/kr/game/sale/controller/IndexController.java
@@ -0,0 +1,18 @@
+package kr.game.sale.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+
+    @GetMapping({"/",""})
+    public String home(){
+        return "index";
+    }
+
+}
---
+++ sale/src/test/java/kr/game/sale/SaleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.game.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
