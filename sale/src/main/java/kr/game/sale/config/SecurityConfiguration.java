325db930fef6f91f4ba14acdd809e3825b9ca330
wonusuh <wonwooseo23@gmail.com>
2024-04-21 02:11:10 +0900

    created : 잘못된 URL을 요청하면 404 페이지를 띄웁니다.

--- sale/src/main/java/kr/game/sale/config/SecurityConfiguration.java
+++ sale/src/main/java/kr/game/sale/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -37,22 +38,34 @@ public class SecurityConfiguration {
                         auth
                                 .requestMatchers("/user/**").authenticated()
                                 .requestMatchers("/admin/**").hasAnyRole("MANAGER", "ADMIN") // 관리자만 접근가능
-                                .anyRequest().permitAll())
+                                .anyRequest().permitAll()
+                )
                 .formLogin(form ->
                         form
                                 .loginPage("/users/loginForm") // 우리가 만든 로그인폼으로 인터셉트됩니다.
                                 .loginProcessingUrl("/userLogin")
                                 .failureHandler(customAuthFailureHandler()) // 로그인실패시 할 작업
-                                .successHandler(customAuthSuccessHandler))
-                .oauth2Login(Customizer.withDefaults())
+                                .successHandler(customAuthSuccessHandler)
+                )
+                .oauth2Login(Customizer.withDefaults()
+                )
                 .logout(logout ->
                         logout
                                 .logoutRequestMatcher(new AntPathRequestMatcher("/users/logout"))
                                 .logoutSuccessUrl("/")
-                                .invalidateHttpSession(true))
+                                .invalidateHttpSession(true)
+                )
                 .exceptionHandling(exceptions ->
                         exceptions
-                                .accessDeniedPage("/"));
+//                                .accessDeniedHandler(accessDeniedHandler())
+                                .accessDeniedPage("/")
+                );
         return http.build();
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        CustomAccessDeniedHandler accessDeniedHandler = new CustomAccessDeniedHandler();
+        accessDeniedHandler.setErrorPage("/denied");
+        return accessDeniedHandler;
     }
 }
