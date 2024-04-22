97a55dd1b28ed3f340808177bfb42e8545cea895
SJL0616 <81796008+SJL0616@users.noreply.github.com>
2024-04-22 12:39:04 +0900

    sns.js수정

--- sale/src/main/resources/static/js/sns.js
+++ sale/src/main/resources/static/js/sns.js
@@ -1,4 +1,26 @@
-function fn_sendFB(sns,game) {
+
+$(document).ready(function(){
+    initKakao();
+});
+
+function initKakao(){
+    Kakao.init('6a5f485741b87f4303214d8ba7957cb3');
+   /* Kakao.Link.createDefaultButton({
+        container: '#btn_kakao',
+        objectType: 'feed',
+        content:{
+            title: '더 좋은 게임즈',
+            description: snsTitle,
+            imageUrl:thisUrl,
+            link:{
+                mobileWebUrl:thisUrl,
+                webUrl:thisUrl
+            }
+        }
+    });*/
+}
+
+function fn_sendFB(sns,game,headerImg) {
     console.log('fn_sendFB');
     var thisUrl = document.URL;
     var snsTitle = "같이 "+game +"해요! with 더 좋은 게임즈";
@@ -10,24 +32,19 @@ function fn_sendFB(sns,game) {
             snsTitle);
         window.open(url, "tweetPop", "width=486, height=286,scrollbars=yes");
     } else if (sns == 'kakao') {
-
-        //카카오 앱 자바스크립트 키
-        Kakao.init('6a5f485741b87f4303214d8ba7957cb3');
-
-        Kakao.Link.createDefaultButton({
-            container: '#btn_kakao',
+        Kakao.Link.sendDefault({
             objectType: 'feed',
             content:{
                 title: '더 좋은 게임즈',
                 description: snsTitle,
-                imageUrl:thisUrl,
+                imageUrl:headerImg,
                 link:{
                     mobileWebUrl:thisUrl,
                     webUrl:thisUrl
                 }
             }
         });
-
+        //카카오 앱 자바스크립트 키
     }else if (sns == 'band'){
         var url = "http://www.band.us/plugin/share?body=" + encodeURIComponent(snsTitle) + "&route=" +
             encodeURIComponent(
