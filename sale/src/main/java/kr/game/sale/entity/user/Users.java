3e49e62f6522829328f03dd5013e99f4e771db85
wonusuh <wonwooseo23@gmail.com>
2024-04-22 10:05:56 +0900

    modified : 유저 엔티티에 주석 삭제

--- sale/src/main/java/kr/game/sale/entity/user/Users.java
+++ sale/src/main/java/kr/game/sale/entity/user/Users.java
@@ -1,13 +1,7 @@
 package kr.game.sale.entity.user;
 
 import jakarta.persistence.*;
-import kr.game.sale.entity.admin.QnA;
-import kr.game.sale.entity.game.review.Review;
 import lombok.Data;
-import net.minidev.json.annotate.JsonIgnore;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
@@ -26,14 +20,4 @@ public class Users {
     // OAuth 를 위해 추가하는 필드
     private String provider;
     private String providerId;
-
-//    // Cart 와의 일대다 관계 설정
-//    @OneToMany(fetch = FetchType.LAZY)
-//    @Transient
-//    private List<Cart> carts;
-//
-//    // QnA 와의 일대다 관계 설정
-//    @OneToMany(fetch = FetchType.LAZY)
-//    @Transient
-//    private List<QnA> qnas;
 }
